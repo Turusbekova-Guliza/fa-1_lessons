ba978a57e591820b4851e13105f3dc21f07bb3e3
Guliza <turusbekovagulisa.6@gmail.com>
2023-05-24 16:26:29 +0600

    '24.04.2023'

---
+++ lesson.1/app.js
@@ -0,0 +1,53 @@
+//ТИПЫ КОВЫЧЕК
+
+// " "  v1
+// ' '  v2
+// ` `  v3
+
+// shift +alt + -> down
+// типы данных
+// number
+// var-> variable озгормо атын озум озгортушум керек 1
+var number1 = 100
+var age = 18
+var usd = 85.45
+usd = 87.45
+var temp = -10
+// string 2
+var userName = "Kanat"
+// boolean ( true | false) 3
+var hasCar = true
+var isTrue = 8 > 4 // true
+// undefined 4
+var empty; // undefined
+// null 5 
+var nullExp = null
+// symbol 6
+var sym = Symbol ('id')
+//object 7 {} []
+var car = {
+    // key : value, ключ : значие 
+    name: 'BMW',
+    color: 'black',
+    price: 20000000,
+    year: 2015,
+}
+console.log(car);
+
+// bigInt 8 ---------------
+var bInt = 4n 
+
+var movies = ['Avatar 2', 'Chas Pik', 'Forsaj 10']
+
+var phone = {
+    nember: 1923043651,
+    name: 'Redmi C9',
+    color: 'black',
+    year: 2018,
+    price: 20000,
+    isTrue: 'New',
+    nullExp: 'Redmi C9',
+    hasCase: false,
+    hasCharger: false,
+}
+console.log(phone);
