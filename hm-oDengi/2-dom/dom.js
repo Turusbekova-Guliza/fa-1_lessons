d706d5a209b486a5118edd0cf9680ca8b40a29c8
Turusbekova-Guliza <134058547+Turusbekova-Guliza@users.noreply.github.com>
2023-05-31 22:06:46 +0600

    o dengi

---
+++ hm-oDengi/2-dom/dom.js
@@ -0,0 +1,22 @@
+const btnForm = document.getElementById("submit");
+const uEmailInput = document.getElementById("email");
+const uPasswordInput = document.getElementById("password");
+
+let userData = {
+  name: "guliza",
+  email: "guliza@gmail.com",
+  password: 123456,
+};
+
+btnForm.onclick = (e) => {
+  e.preventDefault();
+  if (
+    userData.email == uEmailInput.value &&
+    userData.password == uPasswordInput.value
+  ) {
+    alert("Welcome Guliza");
+    window.location.href = "http://127.0.0.1:5500/hm-oDengi/";
+  } else {
+    alert("Email or Password Wrong");
+  }
+};
--- hm-oDengi/main.js
+++ hm-oDengi/main.js
@@ -1,10 +1,53 @@
 const pTag = document.getElementById('balance')
 pTag.innerText = pTag.innerText + '$'
 
-const inputBtn = document.getElementById('input__btn') 
+const inputBtn = document.getElementById('input__btn')
 const btnSend = document.getElementById('send__money')
+const depBtn = document.getElementById('deposit')
+const withBtn = document.getElementById('withdrawal')
+const depInput = document.getElementById('dep-number')
+const withInput = document.getElementById('with-input')
+
+depBtn.onclick = () => {
+    pTag.innerText = parseInt(pTag.innerText) + parseInt(depInput.value) + '$'
+    depInput.value = ''
+}
+
+withBtn.onclick = () => {
+    if(withInput.value > Number(pTag.innerText)) {
+        alert('There is not enough money on your balance. Your balance:' + pTag.innerText)
+    }
+    pTag.innerText = parseInt(pTag.innerText) - parseInt(withInput.value) + '$'
+    withInput.value = ''
+
+}
+
+
 btnSend.onclick = ()  => {
     if (parseInt(pTag.innerText) > 0){
-        pTag.innerText = parseInt(pTag.innerText) - 1 + '$'
+        pTag.innerText = parseInt(pTag.innerText) - Number(inputBtn.value) + '$'
     }
 }
+
+
+const date = new Date()
+const user1 = {
+  name: 'Guliza',
+  lastName: 'Turusbekova',
+  status: 'VIP',
+  date: `0${date.getMonth()}.${date.getDate()}.${date.getFullYear()}`
+}
+// DOM USER ELEMENT
+const userName = document.getElementById('user-name')
+const userDate = document.getElementById('user-date')
+const userStatus = document.getElementById('user-status')
+const userLastName = document.getElementById('user-lastName')
+
+function renderUser(user) {
+  userName.innerText = user.name
+  userDate.innerText = user.date
+  userStatus.innerText = user.status
+  userLastName.innerText = user.lastName
+}
+
+renderUser(user1)
